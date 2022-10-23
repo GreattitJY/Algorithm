cc4d82aa6af0248bcb85892a35fad6cbd3beecb2
greattit <103429329+GreattitJY@users.noreply.github.com>
2022-10-23 18:08:01 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,9 +1,20 @@
-const solution = n => { 
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 0) {
-            sum += i;
-        }
-    }
-    return sum
+// const solution = n => { 
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum
+// }
+
+const solution = n => {
+    let half = n / 2 | 0
+    return half * (half + 1)
 }
+
+// 만약 n 이하의 홀수를 모두 더한다면
+//  const solution = n => {
+//     let half = Math.ceil(n / 2);
+//     return half * half
+// }
