c542f9d99bfb97a0e30eb18959a2941049d50585
jaeyoung Lee <103429329+GreattitJY@users.noreply.github.com>
2023-03-15 16:23:09 +0900

    [level 1] Title: 소수 만들기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,25 +1,29 @@
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+  return results;
+};
+
 function isPrime(num) {
   if (num === 1) return false;
   for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
+    if (num % i === 0) return false;
   }
   return true;
 }
 
 function solution(nums) {
+  const combinationResult = getCombinations(nums, 3);
   let answer = 0;
-  const len = nums.length;
-
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      for (let k = j + 1; k < len; k++) {
-        const sum = nums[i] + nums[j] + nums[k];
-        if (isPrime(sum)) answer += 1;
-      }
-    }
-  }
-
+  combinationResult.forEach((e) => {
+    const sum = e[0] + e[1] + e[2];
+    if (isPrime(sum)) answer++;
+  });
   return answer;
 }
